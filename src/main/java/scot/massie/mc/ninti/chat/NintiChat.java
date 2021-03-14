1b24f52997769295046991c7fc8eaa4c3d60c1be
Chris Massie <chris@massie.scot>
2021-03-14 02:10:58 +0000

    Implemented chat-masking listener
    Added and implemented chat-masking listener.
    
    Uses the permissions ninti.chat.mask.name and ninti.chat.mask.msg

--- src/main/java/scot/massie/mc/ninti/chat/NintiChat.java
+++ src/main/java/scot/massie/mc/ninti/chat/NintiChat.java
@@ -27,6 +27,9 @@ import java.util.stream.Collectors;
 @Mod("nintichat")
 public class NintiChat
 {
+    public static final String PERMISSION_MASK_NAME = "ninti.chat.mask.name";
+    public static final String PERMISSION_MASK_MSG = "ninti.chat.mask.msg";
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -42,8 +45,6 @@ public class NintiChat
     private void setup(final FMLCommonSetupEvent event)
     {
         // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
@@ -51,8 +52,38 @@ public class NintiChat
     public void onServerStarting(FMLServerStartingEvent event)
     {
         // do something when the server starts
+        Permissions.Suggestions.add(PERMISSION_MASK_NAME, PERMISSION_MASK_MSG);
     }
-    
+
+    @SubscribeEvent
+    public void onChat(final ServerChatEvent event)
+    {
+        String namePattern = Permissions.getPlayerPermissionArg(event.getPlayer(), PERMISSION_MASK_NAME);
+        String msgPattern = Permissions.getPlayerPermissionArg(event.getPlayer(), PERMISSION_MASK_MSG);
+
+        if(namePattern == null && msgPattern == null)
+            return;
+
+        if(namePattern == null)
+            namePattern = "<§{name}>§_";
+
+        if(msgPattern == null)
+            msgPattern = "§{msg}";
+
+        namePattern = maskChatLinePart(namePattern, event.getUsername(), event.getMessage());
+        msgPattern  = maskChatLinePart(msgPattern, event.getUsername(), event.getMessage());
+
+        event.setComponent(new StringTextComponent(namePattern + msgPattern));
+    }
+
+    public String maskChatLinePart(String chatLinePart, final String username, final String msg)
+    {
+        chatLinePart = chatLinePart.replaceAll("§\\{name}", username);
+        chatLinePart = chatLinePart.replaceAll("§\\{msg}", msg);
+        chatLinePart = chatLinePart.replaceAll("§_", " ");
+        return chatLinePart;
+    }
+
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents
     {
